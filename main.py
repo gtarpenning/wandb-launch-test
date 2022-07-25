581b6856cde080984e9a988378f3dcffa488ce51
gtarpenning <griffin.tarpenning@wandb.com>
2022-07-25 14:17:38 -0700

    added request usage and os.listdir

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ import requests
 
 wandb.init(project='requests-remote')
 
-# r = requests.get("www.wandb.ai")
+try:
+	r = requests.get("www.wandb.ai")
+except Exception as e:
+	print("Unable to connect to url")
+	print(e)
+
+import os
+
+print(os.listdir())
 print("Belly")
 
 wandb.log({"text-len":121, "code":402})
