6afcdefcf34b8a22ee39519cfa4384a33c966698
gtarpenning <griffin.tarpenning@wandb.com>
2022-07-26 15:43:55 -0700

    try except protection on main

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ import requests
 
 wandb.init(project='requests-remote')
 
-# r = requests.get("www.wandb.ai")
+try:
+	r = requests.get("https://www.wandb.ai")
+	print(f"{r.status_code=} -- {r.text=}")
+except Exception as e:
+	print("Unable to connect to url")
+	print(e)
+
+import os
+
+print(os.listdir())
 print("Belly")
 
 wandb.log({"text-len":121, "code":402})
-
