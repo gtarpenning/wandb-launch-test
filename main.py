00d0852cd34495d3b237f62a828d78cbcd164251
gtarpenning <griffin.tarpenning@wandb.com>
2022-08-03 14:48:57 -0700

    removed annoying text spam

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import wandb
 import requests
 
 try:
-	wandb.init(project='requests-remote', settings=wandb.Settings(start_method="fork"))
+	wandb.init(project='requests-remote')
 except Exception as e: 
 	print("Couldn't load wandb... \n {e}")
 
 import os
 
 r = requests.get("https://docs.wandb.ai/guides/track/launch#init-start-error")
-print(r.text)
+print(r.status_code)
 
 print(os.listdir())
 
