e169b3fee2e97f11e0859a63882972fe1351b7e8
gtarpenning <griffin.tarpenning@wandb.com>
2022-08-03 14:39:32 -0700

    different main setup

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 import wandb
-import requests
-
-wandb.init(project='requests-remote')
 
 try:
-	r = requests.get("https://www.wandb.ai")
-	print(f"{r.status_code=} -- {len(r.text)=}")
-except Exception as e:
-	print("Unable to connect to url")
-	print(e)
+	wandb.init(project='requests-remote')
+except Exception as e: 
+	print("Couldn't load wandb... \n {e}")
 
 import os
 
 print(os.listdir())
-print("Belly")
 
 wandb.log({"text-len":121, "code":402})
