ab2dceaf65108fe3a1b881bb4aa6dc29e20ed7e9
gtarpenning <griffin.tarpenning@wandb.com>
2022-08-03 14:43:52 -0700

    added requests and a fork for wanbd init

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import wandb
+import requests
 
 try:
-	wandb.init(project='requests-remote')
+	wandb.init(project='requests-remote', settings=wandb.Settings(start_method="fork"))
 except Exception as e: 
 	print("Couldn't load wandb... \n {e}")
 
 import os
+
+r = requests.get("https://docs.wandb.ai/guides/track/launch#init-start-error")
+print(r.text)
 
 print(os.listdir())
 
