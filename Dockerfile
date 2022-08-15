a92a697f130ec220c548157104ab25f9cf231548
gtarpenning <griffin.tarpenning@wandb.com>
2022-08-15 15:15:03 -0700

    env vars

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.10.4-slim
 
 RUN pip install -r requirements.txt .
 
-ENV WANDB_BASE_URL=https://api.wandb.test
+ENV WANDB_BASE_URL=http://localhost:9002
 RUN ssl/setup.sh
 RUN ssl/fix-python.sh
 
--- main.py
+++ main.py
@@ -11,6 +11,6 @@ import os
 r = requests.get("https://docs.wandb.ai/guides/track/launch#init-start-error")
 print(r.status_code)
 
-print(os.listdir())
+print(f"{os.listdir()=}")
 
 wandb.log({"text-len":121, "code":402})
